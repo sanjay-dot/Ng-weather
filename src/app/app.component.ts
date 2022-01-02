01e40296d80b138ca48aea4649502ef454d77312
Sanjay_Balaji <70127280+sanjay-dot@users.noreply.github.com>
2022-01-02 21:36:41 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,25 @@ import { Component } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from './services/api.service';
+import {OnInit} from '@angular/core';
 // import { timeStamp } from 'console';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'weather-app';
   closeResult = '';
-  City:any = '';
+  City:any = 'Chennai';
   data:any = {};
   isShown:boolean = true;
   showbutton:boolean = true;
   cityname='';
   countryname ='';
   temp:any ='';
+  settemp:any = '';
   database:any='';
   constructor(private modalService: NgbModal ,private apiservice:ApiService){}
 
@@ -37,38 +40,46 @@ export class AppComponent {
     });
   }
 
+ngOnInit():void{
+  this.submitHandler()
+}
   submitHandler(){
     // console.log(this.City);
     this.apiservice.getdata(this.City).subscribe((res)=>{
         this.data = res;  
-        // console.log(this.data.sys.country);
-        
-        // console.log(this.data.sys.country);
-        // console.log(this.data.name); 
-
         let data:any = {
           City : this.data.name,
           Country:this.data.sys.country,
         }
 
         // console.log(data);
-        
-
         this.apiservice.postdata(data).subscribe(result=>{
           console.log("result" ,result);
-          
-        });
 
         this.apiservice.gettemp(this.cityname).subscribe(res=>{
-          this.database = res;
-          // console.log(this.database);
-        })
+            this.database = res;
+            console.log("result2",this.database);
 
-        this.apiservice.temp(this.database.City).subscribe(res=>{
-          this.temp = res;
-          console.log(this.temp);
-        })
+            for (let elem of this.database) {
+              this.apiservice.temp(elem.City).subscribe(res=>{
+                console.log("temp for city",elem.City,res);
+                elem.weather = res
+                elem.temp = elem.weather.main.temp
+              });
+            }
+          });
+        });
+   ;
     });
+
+    
+
+    // this.apiservice.temp(this.City).subscribe(res=>{
+    //   this.temp = res;
+    //   console.log(this.temp.main.temp);
+    //   this.settemp = this.temp.main.temp
+
+    // })
   }    
 
   }
